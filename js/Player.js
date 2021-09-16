4f9437bffbbe1b224de0248c6cc71f9ecafe5168
cap123ayushman <84783831+cap123ayushman@users.noreply.github.com>
2021-09-16 14:53:29 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,25 @@
+class Player {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true
+      };
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      this.image=loadImage("./assets/player.png")
+      World.add(world, this.body);
+    }
+    display() {
+      var pos = this.body.position;
+      var angle=this.body.angle
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle) 
+      fill("brown");
+      //rectMode(CENTER);
+     // rect(pos.x,pos.y, this.width, this.height);
+     imageMode(CENTER)
+     image(this.image,0,0,this.width,this.height)
+      pop();
+    }
+  }
